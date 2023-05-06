e5a74e055c6d7269a012506da68046d6f3ef004c
Jurgen Verhelst | ez Networking <jurgen.verhelst@ez.be>
2023-05-06 12:49:29 +0200

    test2 script

--- Test2.ps1
+++ Test2.ps1
@@ -1,1 +1,21 @@
-:;n,=lnùlnmù,k
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$Name,
+    
+    [Parameter()]
+    [switch]$Verbose,
+    
+    [Parameter()]
+    [bool]$Enabled
+)
+
+if($Verbose){
+    Write-Output "Verbose mode is enabled."
+}
+
+if($Enabled){
+    Write-Output "$Name is enabled."
+} else {
+    Write-Output "$Name is not enabled."
+}
