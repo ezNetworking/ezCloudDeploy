c064b038f085f26e7ff60e00512522be5250fffe
Jurgen Verhelst | ez Networking <jurgen.verhelst@ez.be>
2023-05-11 22:11:29 +0200

    OEM removed

--- 011_WinPE_PostOS_PrepLocalADezAdminLocalSyncezTools.ps1
+++ 011_WinPE_PostOS_PrepLocalADezAdminLocalSyncezTools.ps1
@@ -93,14 +93,7 @@ $unattendXml = @"
     </settings>
     <settings pass="oobeSystem">
         <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-            <OOBE>
-                <OEMInformation>
-                    <SupportProvider>ez Networking Support</SupportProvider>
-                    <Manufacturer>HP</Manufacturer>
-                    <SupportHours>8/5 to 24/7 depending on contract</SupportHours>
-                    <SupportPhone>+32 3 376 14 25</SupportPhone>
-                    <SupportURL>http://www.ez.be</SupportURL>
-                </OEMInformation>            
+            <OOBE>        
                 <HideEULAPage>true</HideEULAPage>
                 <HideOEMRegistrationScreen>true</HideOEMRegistrationScreen>
                 <HideOnlineAccountScreens>true</HideOnlineAccountScreens>
