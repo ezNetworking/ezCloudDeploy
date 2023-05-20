67a77ab2bced8864826697fab6982697fa301309
Jurgen Verhelst | ez Networking <jurgen.verhelst@ez.be>
2023-05-20 14:47:23 +0200

    updates

--- non_ezCloudDeployGuiScripts/111_Windows_PostOS_DefaultAppsAndOnboard.ps1
+++ non_ezCloudDeployGuiScripts/111_Windows_PostOS_DefaultAppsAndOnboard.ps1
@@ -1,10 +1,31 @@
-Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 Install-Module OSD
+Install-Module OOBEDeploy
+Import-Module OOBEDeploy
 Install-Module burnttoast
 Import-Module burnttoast
 
-# Disable "Do Not Disturb" mode (Quiet Hours)
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\QuietHours" -Name "Enabled" -Value 0
+# Do Not Disturb Off (Dirty Way, not found a better one :) :)
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait("(^{ESC})")   
+Start-Sleep -Milliseconds 500   
+[System.Windows.Forms.SendKeys]::SendWait("(Focus Assist)")   
+Start-Sleep -Milliseconds 200   
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")   
+Start-Sleep -Milliseconds 700  
+[System.Windows.Forms.SendKeys]::SendWait("{TAB} ")   
+Start-Sleep -Milliseconds 700  
+[System.Windows.Forms.SendKeys]::SendWait("{TAB} ")   
+Start-Sleep -Milliseconds 700  
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}")   
+Start-Sleep -Milliseconds 200   
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")   
+Start-Sleep -Milliseconds 700   
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB} ")  
+Start-Sleep -Milliseconds 200   
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}") 
+Start-Sleep -Milliseconds 500     
+[System.Windows.Forms.SendKeys]::SendWait("(%{F4})")    
 
 # Send the toast notification
 $Time = Get-date -Format t
@@ -19,13 +40,22 @@ $Splat = @{
 New-BurntToastNotification @splat 
 
 
+
 Write-Host -ForegroundColor Gray "========================================================================================="
 Start-Transcript -Path "C:\ezNetworking\Automation\Logs\ezCloudDeploy_111_Windows_PostOS_DefaultAppsAndOnboard.log"
 Write-Host -ForegroundColor Gray "========================================================================================="
 
-# Use Start-OOBEDeploy to remove the following apps
-Write-Host -ForegroundColor Cyan "  Zed says: Use Start-OOBEDeploy to remove apps and update Windows "
-Write-Host -ForegroundColor Cyan "            CommunicationsApps,MicrosoftTeams,OfficeHub,People,Skype,Solitaire,Xbox,ZuneMusic,ZuneVideo"
+Write-Host -ForegroundColor Cyan "========================================================================================="
+write-host -ForegroundColor Cyan "Z> User configuration"
+Write-Host -ForegroundColor Cyan "========================================================================================="
+Write-Host -ForegroundColor Gray "Z> Setting ezadminlocal's password to never expire "
+Set-LocalUser -Name "ezAdminLocal" -PasswordNeverExpires $true
+Write-Host -ForegroundColor Cyan "========================================================================================="
+
+write-host -ForegroundColor Cyan "Z> Removing apps and updating Windows"
+Write-Host -ForegroundColor Cyan "========================================================================================="
+Write-Host -ForegroundColor Gray "Z> Use Start-OOBEDeploy to remove apps and update Windows "
+Write-Host -ForegroundColor Gray "   CommunicationsApps,MicrosoftTeams,OfficeHub,People,Skype,Solitaire,Xbox,ZuneMusic,ZuneVideo"
 $Params = @{
     Autopilot = $false
     RemoveAppx = "CommunicationsApps","OfficeHub","People","Skype","Solitaire","Xbox","ZuneMusic","ZuneVideo"
@@ -34,30 +64,34 @@ $Params = @{
 }
 Start-OOBEDeploy @Params
 
+Write-Host -ForegroundColor Cyan "========================================================================================="
+write-host -ForegroundColor Cyan "Z> Installing apps and onboarding client to ezRmm"
+Write-Host -ForegroundColor Cyan "========================================================================================="
+
 # Install Choco and minimal default packages
 Write-Host -ForegroundColor Gray "========================================================================================="
-write-host " Zed says: Installing Chocolatey"
+write-host "Z> Installing Chocolatey"
 
 try {
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     Start-Sleep -s 30
 }
 catch {
-    Write-Error " Zed says: Chocolatey is already installed or had an error $($_.Exception.Message)"
+    Write-Error "Z> Chocolatey is already installed or had an error $($_.Exception.Message)"
 }
 
 # -y confirm yes for any prompt during the install process
-write-host " Zed says: Installing Chocolatey packages"
+write-host "Z> Installing Chocolatey packages"
 choco install googlechrome -y
 choco install treesizefree -y
 choco install tailblazer -y
 Write-Host -ForegroundColor Gray "========================================================================================="
 
 # Install ezRmm and ezRS
-write-host " Zed says: reading the ezClientConfig.json file"
+write-host "Z> reading the ezClientConfig.json file"
 $ezClientConfig = Get-Content -Path "C:\ezNetworking\Automation\ezCloudDeploy\ezClientConfig.json" | ConvertFrom-Json
 
-write-host " Zed says: Downloading ezRmmInstaller.msi and installing it for customer $($ezClientConfig.ezRmmId)"
+write-host "Z> Downloading ezRmmInstaller.msi and installing it for customer $($ezClientConfig.ezRmmId)"
 $Splat = @{
     Text = 'Zed: Installing ez RMM' , "Downloading and installing... Started $Time"
     Applogo = 'https://iili.io/H8B8JtI.png'
@@ -67,18 +101,18 @@ New-BurntToastNotification @splat
 
 try {
     $ezRmmUrl = "http://support.ez.be/GetAgent/Msi/?customerId=$($ezClientConfig.ezRmmId)" + '&integratorLogin=jurgen.verhelst%40ez.be'
-    write-host " Zed says: Downloading ezRmmInstaller.msi from $ezRmmUrl"
+    write-host "Z> Downloading ezRmmInstaller.msi from $ezRmmUrl"
     Invoke-WebRequest -Uri $ezRmmUrl -OutFile "C:\ezNetworking\Automation\ezCloudDeploy\ezRmmInstaller.msi"
     Start-Process -FilePath "C:\ezNetworking\Automation\ezCloudDeploy\ezRmmInstaller.msi" -ArgumentList "/quiet" -Wait
     
 }
 catch {
-    Write-Error " Zed says: ezRmm is already installed or had an error $($_.Exception.Message)"
+    Write-Error "Z> ezRmm is already installed or had an error $($_.Exception.Message)"
 }
 
 Write-Host -ForegroundColor Gray "========================================================================================="
-write-host " Zed says: Downloading and installing ezRS"
-write-host " Zed says: Downloading ezRmmInstaller.msi and installing it"
+write-host -ForegroundColor Gray "Z> Downloading and installing ezRS"
+write-host -ForegroundColor Gray "Z> Downloading ezRmmInstaller.msi and installing it"
 $Splat = @{
     Text = 'Zed: Installing ez Remote Support' , "Downloading and installing... Started $Time"
     Applogo = 'https://iili.io/H8B8JtI.png'
@@ -92,7 +126,7 @@ try {
     Start-Process -FilePath "C:\ezNetworking\Automation\ezCloudDeploy\ezRsInstaller.exe" -ArgumentList "/S" -Wait
 }
 catch {
-    Write-Error " Zed says: ezRS is already installed or had an error $($_.Exception.Message)"
+    Write-Error "Z> ezRS is already installed or had an error $($_.Exception.Message)"
 }
 Write-Host -ForegroundColor Gray "========================================================================================="
 
