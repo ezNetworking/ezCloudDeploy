4181d903b1b5c748e6de58ead3362977f7ec0b99
Jurgen Verhelst | ez Networking <jurgen.verhelst@ez.be>
2023-07-07 23:16:40 +0200

    toasts added

--- non_ezCloudDeployGuiScripts/113_Windows_PostOS_ThinClientCustomisations copy.ps1
+++ non_ezCloudDeployGuiScripts/113_Windows_PostOS_ThinClientCustomisations copy.ps1
@@ -8,6 +8,8 @@ Start-Transcript -Path "C:\ezNetworking\Automation\Logs\ezCloudDeploy_113_Window
 Write-Host -ForegroundColor Gray "========================================================================================="
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module burnttoast
+Import-Module burnttoast
 
 # Define the Variables
 $jsonFilePath = 'C:\ezNetworking\Automation\ezCloudDeploy\ezClientConfig.json'
@@ -18,15 +20,95 @@ $layoutFilePath = "C:\ezNetworking\Automation\ezCloudDeploy\StartMenuTaskbarLayo
 $userName = "User"
 $userGroupName = "NonAdminUsers"
 
+write-host "Z> Setting Focus Assist to Off"
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait("(^{ESC})")   
+Start-Sleep -Milliseconds 500   
+[System.Windows.Forms.SendKeys]::SendWait("(Focus Assist)")   
+Start-Sleep -Milliseconds 200   
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")   
+Start-Sleep -Milliseconds 700  
+[System.Windows.Forms.SendKeys]::SendWait("{TAB} ")   
+Start-Sleep -Milliseconds 700  
+[System.Windows.Forms.SendKeys]::SendWait("{TAB} ")   
+Start-Sleep -Milliseconds 700  
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}")   
+Start-Sleep -Milliseconds 200   
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")   
+Start-Sleep -Milliseconds 700   
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB} ")  
+Start-Sleep -Milliseconds 200   
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}") 
+Start-Sleep -Milliseconds 500     
+[System.Windows.Forms.SendKeys]::SendWait("(%{F4})")  
+
+
 # Load the JSON file
+$ezClientConfig = Get-Content -Path $jsonFilePath | ConvertFrom-Json
+
+# Install ezRmm and ezRS
+write-host -ForegroundColor White "Z> ezRMM - Downloading and installing it for customer $($ezClientConfig.ezRmmId)"
+$Time = Get-date -Format t
+$Splat = @{
+    Text = 'Zed: Installing ez RMM' , "Downloading and installing... Started $Time"
+    Applogo = 'https://iili.io/H8B8JtI.png'
+    Sound = 'IM'
+}
+New-BurntToastNotification @splat 
+
+
+try {
+    $ezRmmUrl = "http://support.ez.be/GetAgent/Msi/?customerId=$($ezClientConfig.ezRmmId)" + '&integratorLogin=jurgen.verhelst%40ez.be'
+    write-host -ForegroundColor Gray "Z> Downloading ezRmmInstaller.msi from $ezRmmUrl"
+    Invoke-WebRequest -Uri $ezRmmUrl -OutFile "C:\ezNetworking\Automation\ezCloudDeploy\ezRmmInstaller.msi"
+    # Send the toast Alarm
+    $Btn = New-BTButton -Content 'Got it!' -arguments 'ok'
+    $Splat = @{
+        Text = 'Zed: ezRMM needs your Attention' , "Please press OK."
+        Applogo = 'https://iili.io/H8B8JtI.png'
+        Sound = 'Alarm10'
+        Button = $Btn
+        HeroImage = 'https://iili.io/HU7A5bV.jpg'
+}
+New-BurntToastNotification @splat
+
+    Start-Process -FilePath "C:\ezNetworking\Automation\ezCloudDeploy\ezRmmInstaller.msi" -ArgumentList "/quiet" -Wait
+    
+}
+catch {
+    Write-Error -ForegroundColor Gray "Z> ezRmm is already installed or had an error $($_.Exception.Message)"
+}
+
+
+Write-Host -ForegroundColor Gray "========================================================================================="
+write-host -ForegroundColor Gray "Z> ezRS - Downloading and installing it"
+$Splat = @{
+    Text = 'Zed: Installing ez Remote Support' , "Downloading and installing... Started $Time"
+    Applogo = 'https://iili.io/H8B8JtI.png'
+    Sound = 'IM'
+}
+New-BurntToastNotification @splat 
+
+# Need Fix ezRsInstaller is only 10kb big...
+try {
+    $ezRsUrl = 'https://get.teamviewer.com/ezNetworkingHost'
+    Invoke-WebRequest -Uri $ezRsUrl -OutFile "C:\ezNetworking\Automation\ezCloudDeploy\ezRsInstaller.exe"
+    Start-Process -FilePath "C:\ezNetworking\Automation\ezCloudDeploy\ezRsInstaller.exe" -ArgumentList "/S" -Wait
+}
+catch {
+    Write-Error "Z> ezRS is already installed or had an error $($_.Exception.Message)"
+}
+
+
+
 Write-Host -ForegroundColor White "========================================================================================="
 Write-Host -ForegroundColor White "Z> RDS shortcut creation."
 Write-Host -ForegroundColor White "========================================================================================="
 Write-Host -ForegroundColor Gray "Z> Loading ClientConfig JSON."
-$jsonContent = Get-Content -Path $jsonFilePath -Raw | ConvertFrom-Json
+
 
 # Get the RDS URI from the JSON file
-$rdsUri = $jsonContent.ezClientConfig.custRdsUri
+$rdsUri = $ezClientConfig.custRdsUri
 
 # Delete all links in the default public user's desktop
 Get-ChildItem -Path $desktopFolderPath -Filter '*.*' -File | Remove-Item -Force
